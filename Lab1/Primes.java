b4c8efb22bf75ac5f9a84255ce96aa18006b980b
ProstoArtems <105981938+ProstoArtems@users.noreply.github.com>
2024-09-17 01:35:49 +0300

    Update Primes.java

--- Lab1/Primes.java
+++ Lab1/Primes.java
@@ -1,16 +1,17 @@
 public class Primes {
     public static void main(String[] args) {
         for(int i =2; i<=100; i++) {
-            boolean isPrime = true;
-            for(int j=2; j<=Math.sqrt(i); j++) {
-                if(i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if(isPrime == true) {
+            if (isPrime(i)){
                 System.out.println(i);
             }
         }
+    }
+    public static boolean isPrime(int n){
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
